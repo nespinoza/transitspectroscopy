672f81f999eee9ea317986ee88c2f2150957d4ce
Nestor Espinoza <nespinoza@stsci.edu>
2022-03-02 15:21:38 -0500

    added C-version of CCF

---
+++ setup.py
@@ -0,0 +1,37 @@
+import re
+import numpy
+#from distutils.core import setup, Extension
+from setuptools import setup, Extension
+
+VERSIONFILE='src/_version.py'
+verstrline = open(VERSIONFILE, "rt").read()
+VSRE = r"^__version__ = ['\"]([^'\"]*)['\"]"
+mo = re.search(VSRE, verstrline, re.M)
+if mo:
+    verstr = mo.group(1)
+else:
+    raise RuntimeError("Unable to find version string in %s." % (VERSIONFILE,))
+
+marsh_module = Extension('Marsh', 
+                         sources = ['src/c-code/OptimalExtraction/Marsh.c'], 
+                         libraries=['gsl', 'gslcblas', 'm'], 
+                         include_dirs=[numpy.get_include(),'/usr/local/include'])
+
+ccf_module = Extension('CCF',
+                       sources = ['src/c-code/Utilities/CCF.c'],
+                       libraries=['gsl', 'gslcblas', 'm'],
+                       include_dirs=[numpy.get_include(),'/usr/local/include'])
+
+setup(name='transitspectroscopy',
+      version=verstr,
+      description='transitspectroscopy: a library for all your transit spectroscopy needs',
+      url='http://github.com/nespinoza/transitspectroscopy',
+      author='Nestor Espinoza',
+      author_email='nespinoza@stsci.edu',
+      license='MIT',
+      packages=['transitspectroscopy'],
+      package_dir={'transitspectroscopy': 'src'},
+      install_requires=['numpy','scipy', 'jwst', 'astropy'],
+      python_requires='>=3.0',
+      ext_modules = [marsh_module, ccf_module],
+      zip_safe=False)
