0df5684b56b97009b9ccb689b236a3f993dd145b
Nestor Espinoza <nespinoza@stsci.edu>
2022-05-04 16:40:59 -0400

    fixed bug on jwst

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,7 @@
 from ._version import __version__
-__all__ = ['spectroscopy', 'utils','transitfitting']
+__all__ = ['spectroscopy', 'utils', 'transitfitting', 'jwst']
 
 from .spectroscopy import *
 from .transitfitting import *
 from .utils import *
+from .jwst import *
